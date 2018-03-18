6fd033499738dbdd8c66cc528a63871a5a46b66d
Mateus Alves <mateusmpa@gmail.com>
2018-03-17 23:09:08 -0300

    Creates services using fae scafflod

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180318020214) do
+ActiveRecord::Schema.define(version: 20180318020758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -181,6 +181,15 @@ ActiveRecord::Schema.define(version: 20180318020214) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.index ["supplier_id"], name: "index_products_on_supplier_id", using: :btree
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.decimal  "price"
+    t.text     "observations"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "suppliers", force: :cascade do |t|
